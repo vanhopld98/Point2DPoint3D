9a3b073940a22c4580767c18bee5a939906b3150
vanhopld98 <vanhopld98@gmail.com>
2021-08-11 15:19:55 +0700

    point

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(1,2);
+        System.out.println(point2D);
+
+        Point3D point3D = new Point3D(1,2,3);
+        System.out.println(point3D);
+    }
+}
